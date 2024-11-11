b97d912d7652bc9351505cbb9c30ba3f6b24abdb
kow111 <chome710@gmail.com>
2024-11-11 18:04:30 +0700

    minor fix

--- lib/screens/auth/forgot_password_screen.dart
+++ lib/screens/auth/forgot_password_screen.dart
@@ -1,5 +1,4 @@
 import 'package:app_manager/features/auth/forget_password/cubit/forgot_password_cubit.dart';
-import 'package:app_manager/features/auth/otp/cubit/otp_cubit.dart';
 import 'package:app_manager/repositories/auth_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
